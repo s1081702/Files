a63cae638eb8505ef5c5b04fb155a53925724e78
longsshen <s1082823@gm.pu.edu.tw>
2021-06-13 00:54:55 +0800

    蓉公公故事介面

---
+++ app/src/main/java/tw/edu/pu/s1081702/files/StoryActivity.kt
@@ -0,0 +1,18 @@
+package tw.edu.pu.s1081702.files
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class StoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_story)
+    }
+
+    fun story1Tapped(view: View) {
+
+    }
+
+    fun story2Tapped(view: View) {}
+}
