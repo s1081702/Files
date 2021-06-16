9864b24a045191809b9450ebd4054badfa791407
longsshen <s1082823@gm.pu.edu.tw>
2021-06-17 00:38:14 +0800

    蓉公公地圖連結故事介面修稿中

--- app/src/main/java/tw/edu/pu/s1081702/files/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1081702/files/MainActivity.kt
@@ -1,11 +1,26 @@
 package tw.edu.pu.s1081702.files
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        TaichungB.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(p0: View) {
+                intent = Intent(this@MainActivity, StoryActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
     }
+
+
+
 }
